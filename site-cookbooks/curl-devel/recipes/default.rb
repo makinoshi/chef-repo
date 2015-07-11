c0e9dae8930c719283c9463cde08658e22f0b8c3
makinoshi <masa.kinoshita02@gmail.com>
2015-07-12 00:38:33 +0900

    Add curl-devel recipe

---
+++ site-cookbooks/curl-devel/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: curl-devel
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "curl-devel" do 
+  action :install
+end
